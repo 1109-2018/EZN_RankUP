b172422fc372c37742461d2405cac51fa0ca2af7
1109-2018 <58660843+1109-2018@users.noreply.github.com>
2019-12-27 13:27:02 +0000

    Adicionado sqlite e novo metodo de conexão

--- src/pt/elevenzeronine/rankup/database/ConnectionManager.java
+++ src/pt/elevenzeronine/rankup/database/ConnectionManager.java
@@ -1,78 +1,67 @@
 package pt.elevenzeronine.rankup.database;
 
+import org.bukkit.Bukkit;
+import pt.elevenzeronine.rankup.RankupPlugin;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
-
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class ConnectionManager {
 
-    private Connection connection;
-    private Properties properties;
+    public static Connection connection = null;
 
-    private String tableName;
-    private String pluginName;
-
-    private String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
-    private final String MAX_POOL = "250";
-
-    public ConnectionManager(JavaPlugin plugin, String parameters, String address, String username, String password,
-                           String databaseName,String tableName) {
-
-        this.pluginName = plugin.getName();
-        this.tableName = tableName;
+    public static void openMySqlConnection() {
+        String USER = RankupPlugin.getPlugin().mysql.getString("Username");
+        String PASSWORD = RankupPlugin.getPlugin().mysql.getString("Password");
+        String DATABASE = RankupPlugin.getPlugin().mysql.getString("Database");
+        String HOST = RankupPlugin.getPlugin().mysql.getString("Host");
+        int PORT = 3306;
 
         try {
-            String USERNAME = username;
-            String PASSWORD = password;
-            String DATABASE_URL = "jdbc:mysql://"+address+":3306/"+databaseName;
-
-            Class.forName(DATABASE_DRIVER);
-            connection = DriverManager.getConnection(DATABASE_URL, getProperties(USERNAME,PASSWORD));
-
-            Statement stmt = connection.createStatement();
-
-            String sql = "CREATE TABLE IF NOT EXISTS " + tableName + " (" + parameters + ");";
-
-            stmt.execute(sql);
-            stmt.close();
-            Bukkit.getConsoleSender().sendMessage("§7[EZN_RankUP] §eMySql conectado!");
-        } catch (Exception e) {
-            Bukkit.getConsoleSender()
-                    .sendMessage("§7[" + pluginName.toUpperCase() + "] §cOcorreu um erro ao tentar conectar ao mysql");
+            connection = DriverManager.getConnection("jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE, USER, PASSWORD);
+            createTable();
+            Bukkit.getConsoleSender().sendMessage("§7[EZN_RANKUP] §eMySQL carregado com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Bukkit.getConsoleSender().sendMessage("§7[EZN_RANKUP] §cErro ao carregar MySQL!");
         }
     }
 
-
-    private Properties getProperties(String USERNAME, String PASSWORD) {
-        if (properties == null) {
-            properties = new Properties();
-            properties.setProperty("user", USERNAME);
-            properties.setProperty("password", PASSWORD);
-            properties.setProperty("MaxPooledStatements", MAX_POOL);
-            properties.setProperty("connectTimeout", "0");
-            properties.setProperty("socketTimeout", "0");
+    public static void openSQLiteConnection() {
+        File file = new File(RankupPlugin.getPlugin().getDataFolder(), "rankup.db");
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + file);
+            createTable();
+            Bukkit.getConsoleSender().sendMessage("§7[EZN_RANKUP] §eSQLite carregado com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Bukkit.getConsoleSender().sendMessage("§7[EZN_RANKUP] §cErro no SQLite contacte-me no discord!");
         }
-        return properties;
     }
 
-    public String getTableName() {
-        return tableName;
+    public static void createTable() {
+        PreparedStatement stm = null;
+
+        try {
+            stm = connection.prepareStatement("CREATE TABLE IF NOT EXISTS `ezn_rankup` (`player` VARCHAR(24) NULL, `uuid` VARCHAR(60) NULL, `rank` VARCHAR(250))");
+            stm.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public Connection getConnection() {
-        return connection;
-    }
-
-
-    public void close() throws SQLException {
-        connection.close();
+    public static void close() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
-
